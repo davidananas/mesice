eab4b3bac3f831f47f1c701bfbe016e1a2af81dd
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-11 17:27:46 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,59 @@
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    mesic += -1
-    if (mesic < 0) {
-        mesic = 0
+    if (m_a_d == "mesice") {
+        basic.clearScreen()
+        mesic += -1
+        if (mesic < 0) {
+            mesic = 0
+        }
+        basic.showString("" + (mesice[mesic]))
+    } else {
+        basic.clearScreen()
+        kolik += -1
+        if (kolik < 0) {
+            kolik = 0
+        }
+        basic.showString("" + (dny[kolik]))
     }
-    basic.showString("" + (seznam[mesic]))
+})
+input.onButtonPressed(Button.AB, function () {
+    if (a_b == 0) {
+        m_a_d = "dny"
+    } else if (a_b == 1) {
+        m_a_d = "mesice"
+    }
+    if (a_b == 0) {
+        a_b += 1
+    } else if (a_b == 1) {
+        a_b += -1
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    mesic += 1
-    if (mesic > 11) {
-        mesic = 11
+    if (m_a_d == "mesice") {
+        basic.clearScreen()
+        mesic += 1
+        if (mesic > 11) {
+            mesic = 11
+        }
+        basic.showString("" + (mesice[mesic]))
+    } else {
+        basic.clearScreen()
+        kolik += 1
+        if (kolik > 6) {
+            kolik = 6
+        }
+        basic.showString("" + (dny[kolik]))
     }
-    basic.showString("" + (seznam[mesic]))
 })
+let dny: string[] = []
 let mesic = 0
-let seznam: string[] = []
-seznam = [
+let mesice: string[] = []
+let m_a_d = ""
+let kolik = 0
+let a_b = 0
+a_b = 0
+kolik = 0
+m_a_d = "mesice"
+mesice = [
 "LEDEN",
 "UNOR",
 "BREZEN",
@@ -31,3 +68,12 @@ seznam = [
 "PROSINEC"
 ]
 mesic = 0
+dny = [
+"PONDELI",
+"UTERY",
+"STREDA",
+"CTVRTEK",
+"PATEK",
+"SOBOTA",
+"NEDELE"
+]
